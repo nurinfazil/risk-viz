641c3fad068a5aa119d2780cb301d4c0e6a54d5a
Nurin Fazil <nurinfazil@Nurins-MacBook-Air.local>
2023-05-04 23:58:21 -0400

    Chart done. Components connected

--- src/app/pages/landingPage.tsx
+++ src/app/pages/landingPage.tsx
@@ -8,7 +8,7 @@ import Papa, { ParseResult } from "papaparse";
 import MapRender from "@/components/MapRender";
 import DataTable from "@/components/DataTable";
 import { Dropdown } from "flowbite-react";
-import ChartSection from "@/components/ChartSection";
+import DataChart from "@/components/DataChart";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +17,13 @@ export default function LandingPage() {
   const [values, setValues] = useState<[] | undefined>();
   const [decade, setDecade] = useState<number>(0);
   const [headerNames, setHeaderNames] = useState<string[]>([]);
+  const [uniqueLatLong, setUniqueLatLong] = useState<[]>([]);
+  const [uniqueAsset, setUniqueAsset] = useState<[]>([]);
+  const [uniqueBusiness, setUniqueBusiness] = useState<[]>([]);
+  const [selectedLocation, setSelectedLocation] = useState<any>("All");
+  const [selectedAsset, setSelectedAsset] = useState<string>("All");
+  const [selectedBusiness, setSelectedBusiness] = useState<string>("All");
+  const [filteredData, setFilteredData] = useState<[]>([]);
   const riskFactorNames = [
     "Earthquake",
     "Extreme heat",
@@ -46,17 +53,6 @@ export default function LandingPage() {
             setHeaderNames(headerNamesCopy);
           }
         }
-        // Convert headers to camel case
-        // let headerArray = headerName.split(" ");
-        // if (headerArray.length > 1) {
-        //   let modHeaderName =
-        //     headerArray[0].toLowerCase() +
-        //     headerArray[1].slice(0, 1).toUpperCase() +
-        //     headerArray[1].slice(1);
-        //   return modHeaderName;
-        // } else {
-        //   return headerName.toLowerCase();
-        // }
 
         return headerName;
       },
@@ -88,6 +84,75 @@ export default function LandingPage() {
     });
   }, []);
 
+  useEffect(() => {
+    // get unique values for Location, Asset Name and Business Category
+    if (values) {
+      const allLatLong = values.map((row) => {
+        return [row["Lat"], row["Long"]];
+      });
+
+      let uniqueLatLong = Array.from(
+        new Set(allLatLong.map(JSON.stringify))
+      ).map(JSON.parse);
+
+      setUniqueLatLong(["All"].concat(uniqueLatLong));
+
+      const allAsset = values.map((row) => {
+        return row["Asset Name"];
+      });
+
+      let uniqueAsset: Set<number> = new Set<number>(allAsset);
+      setUniqueAsset(["All"].concat(Array.from(uniqueAsset.values())));
+
+      const allBusiness = values.map((row) => {
+        return row["Business Category"];
+      });
+
+      let uniqueBusiness: Set<number> = new Set<number>(allBusiness);
+      setUniqueBusiness(["All"].concat(Array.from(uniqueBusiness.values())));
+
+      setFilteredData(values);
+    }
+  }, [values]);
+
+  useEffect(() => {
+    var modifiedData: any = values;
+
+    // Filter by decade
+    // if (decade !== 0) {
+    //   modifiedData = modifiedData.filter((row: any) => {
+    //     const year = parseInt(row["Year"]);
+    //     return year == decade;
+    //   });
+    // }
+
+    // Filter by location
+    if (selectedLocation !== "All") {
+      modifiedData = modifiedData.filter((row: any) => {
+        const lat = row["Lat"];
+        const long = row["Long"];
+
+        return selectedLocation[0] == lat && selectedLocation[1] == long;
+      });
+    }
+
+    // Filter by asset name
+    if (selectedAsset !== "All") {
+      modifiedData = modifiedData.filter((row: any) => {
+        return selectedAsset == row["Asset Name"];
+      });
+    }
+
+    // Filter by business name
+    if (selectedBusiness !== "All") {
+      modifiedData = modifiedData.filter((row: any) => {
+        return selectedBusiness == row["Business Category"];
+      });
+    }
+
+    setFilteredData(modifiedData);
+  }, [selectedLocation, selectedBusiness, selectedAsset]);
+
   let allYears: number[] = [];
   let uniqueYears: Set<number> = new Set([]);
 
@@ -101,6 +166,18 @@ export default function LandingPage() {
 
   const handleDropdownSelect = (year) => {
     setDecade(year);
+  };
+
+  const handleChartDropdownSelect = (selectedVal: any, type: string) => {
+    if (type == "location") {
+      setSelectedLocation(selectedVal);
+    }
+    if (type == "asset") {
+      setSelectedAsset(selectedVal);
+    }
+    if (type == "business") {
+      setSelectedBusiness(selectedVal);
+    }
   };
 
   return (
@@ -130,11 +207,73 @@ export default function LandingPage() {
       {values ? <MapRender data={values} decade={decade} /> : null}
 
       <div>
-        <DataTable data={values} decade={decade} headerNames={headerNames} />
+        <DataTable
+          data={filteredData}
+          decade={decade}
+          headerNames={headerNames}
+        />
       </div>
 
       <div className="pt-5">
-        <ChartSection data={values} decade={decade} />
+        <div className="flex justify-evenly pb-5">
+          <Dropdown
+            label={
+              selectedLocation == "All"
+                ? "Location (All)"
+                : `${selectedLocation[0]}, ${selectedLocation[1]}`
+            }
+          >
+            {uniqueLatLong.map((location) => {
+              return (
+                <Dropdown.Item
+                  key={`${location[0]}, ${location[1]}`}
+                  onClick={() => {
+                    handleChartDropdownSelect(location, "location");
+                  }}
+                >
+                  {location == "All" ? "All" : `${location[0]}, ${location[1]}`}
+                </Dropdown.Item>
+              );
+            })}
+          </Dropdown>
+          <Dropdown
+            label={selectedAsset == "All" ? "Asset Name (All)" : selectedAsset}
+          >
+            {uniqueAsset.map((asset) => {
+              return (
+                <Dropdown.Item
+                  key={asset}
+                  onClick={() => {
+                    handleChartDropdownSelect(asset, "asset");
+                  }}
+                >
+                  {asset}
+                </Dropdown.Item>
+              );
+            })}
+          </Dropdown>
+          <Dropdown
+            label={
+              selectedBusiness == "All"
+                ? "Business Category (All)"
+                : selectedBusiness
+            }
+          >
+            {uniqueBusiness.map((business) => {
+              return (
+                <Dropdown.Item
+                  key={business}
+                  onClick={() => {
+                    handleChartDropdownSelect(business, "business");
+                  }}
+                >
+                  {business}
+                </Dropdown.Item>
+              );
+            })}
+          </Dropdown>
+        </div>
+        <DataChart filteredData={filteredData} />
       </div>
     </div>
   );
--- src/components/DataChart.tsx
+++ src/components/DataChart.tsx
@@ -1,16 +1,128 @@
 import React from "react";
+import { Scatter, Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
 interface DataChartProps {
   filteredData: [];
 }
 
+ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
+
 const DataChart: React.FC<DataChartProps> = ({ filteredData }) => {
-  // destructure any necessary props here
-  // implement the component logic here
+  function transformDataForScatter(data: any) {
+    let reformattedData = [];
 
-  console.log(filteredData);
+    if (data) {
+      reformattedData = data.map((row) => {
+        return { x: row["Year"], y: row["Risk Rating"] };
+      });
+    }
 
-  return <div>Chart</div>;
+    return reformattedData;
+  }
+
+  function transformDataForLine(data: any) {
+    console.log(data);
+    let averageRiskRatingData: { x: any; y: any }[] = [];
+
+    if (data) {
+      const reformattedData = data.reduce((acc: {}, row: any) => {
+        const year = row["Year"];
+        const riskRating = parseFloat(row["Risk Rating"]);
+        if (!acc[year]) {
+          acc[year] = {
+            year: year,
+            sum: riskRating,
+            count: 1,
+          };
+        } else {
+          acc[year].sum += riskRating;
+          acc[year].count++;
+        }
+        return acc;
+      }, {});
+
+      averageRiskRatingData = Object.values(reformattedData).map(
+        (item: any) => ({
+          x: item.year,
+          y: (item.sum / item.count).toFixed(2),
+        })
+      );
+    }
+
+    return averageRiskRatingData;
+  }
+
+  transformDataForLine(filteredData);
+
+  const data = {
+    labels: ["Scatter", "Line"],
+    datasets: [
+      {
+        label: "Risk Rating",
+        data: transformDataForScatter(filteredData),
+        backgroundColor: "rgba(138,180,248, 0.2)",
+        borderColor: "rgba(138,180,248, 1)",
+        borderWidth: 2,
+        type: "scatter",
+      },
+      {
+        label: "Risk Rating Average by Decade",
+        data: transformDataForLine(filteredData),
+        backgroundColor: "rgba(25,86,219, 0.2)",
+        borderColor: "rgba(25,86,219, 1)",
+        borderWidth: 2,
+        type: "line",
+      },
+    ],
+  };
+  const options = {
+    responsive: true,
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: "Decade",
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: "Risk Rating",
+        },
+      },
+    },
+    backgroundColor: "white",
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: function (tooltipItem) {
+            const assetName = filteredData[tooltipItem.dataIndex]["Asset Name"];
+            const riskRating =
+              filteredData[tooltipItem.dataIndex]["Risk Rating"];
+            const riskFactors = JSON.stringify(
+              filteredData[tooltipItem.dataIndex]["Risk Factors"]
+            );
+            const year = filteredData[tooltipItem.dataIndex]["Year"];
+            return `Asset Name: ${assetName}, Risk Rating: ${riskRating}, Risk Factors: ${riskFactors}, Year: ${year}`;
+          },
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="bg-white">
+      <Scatter data={data} options={options} />
+    </div>
+  );
 };
 
 export default DataChart;
