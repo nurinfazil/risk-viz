8795a3446710d7435703b04e4dc2abc107edbcf7
Nurin Fazil <nurinfazil@Nurins-MacBook-Air.local>
2023-08-21 16:19:13 -0400

    Updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "RiskThinking.AI Work Sample | Nurin Fazil",
-  description: "RiskThiking.AI Work Sample by Nurin Fazil",
+  title: "Risk Visualization in Canada | Nurin Fazil",
+  description: "Risk Visualization in Canada by Nurin Fazil",
 };
 
 export default function RootLayout({
